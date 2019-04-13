a950b530a854c00914f029993c5d77dd5fb1d8ff
Ja Dim Zahkung <dim@Jas-MacBook-Air.local>
2019-04-13 13:17:50 +0630

    fixed modal-guide

--- includes/modal-guide_select.php
+++ includes/modal-guide_select.php
@@ -14,7 +14,7 @@
         <!-- modal-body -->
         <div class="modal-body">
             <?php
-            $rows_tour_guides_modal = table_tour_guides('select', NULL);
+            $rows_tour_guides_modal = table_tour_guides('select_all', NULL, NULL);
             foreach ($rows_tour_guides_modal as $row_tour_guides_modal) {
                 echo "<button value=\"$row_tour_guides_modal->Id\" id=\"$row_tour_guides_modal->Id\" onclick=\"selectGuide($row_tour_guides_modal->Id)\">".$row_tour_guides_modal->Name." - ".$row_tour_guides_modal->Language."</button>";
             }
